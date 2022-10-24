a8717aa64702ae88fd7f0602f10957d3e1a7677d
MoniReach0902 <97006297+MoniReach0902@users.noreply.github.com>
2022-10-24 19:12:47 +0700

    dgdf

--- resources/lang/en/dev.php
+++ resources/lang/en/dev.php
@@ -1,37 +1,30 @@
 <?php
 
 return [
-<<<<<<< HEAD
-    "category" => "Category",
+    "name_kh_eng" => "Name English & Khmer",
+    "kh" => "Khmer",
+    "Eng" => "English",
+    "new" => "New",
     "new_category" => "New Category",
-    'lang_en' => 'Englist',
-    'lang_kh' => 'Khmer',
-=======
-    "name_kh_eng"=>"Name English & Khmer",
-    "kh"=>"Khmer",
-    "Eng"=>"English",
-    "new"=>"New",
-    "new_category"=>"New Category",
-    "all_category"=>"All Category",
-    "category"=>"Category",
-    "search"=>"Search",
-    "year"=>"Year",
-    "product"=>"Product",
-    "new_product"=>"New Product",
-    "all_product"=>"All Product",
-    "product_management"=>"Product Management",
-    "permission"=>"Permission",
-    "user"=>"User",
+    "all_category" => "All Category",
+    "category" => "Category",
+    "search" => "Search",
+    "year" => "Year",
+    "product" => "Product",
+    "new_product" => "New Product",
+    "all_product" => "All Product",
+    "product_management" => "Product Management",
+    "permission" => "Permission",
+    "user" => "User",
 
-    "customer"=>"Customers",
-    "new_customer"=>"New Customer",
-    "all_customer"=>"All Customers",
+    "customer" => "Customers",
+    "new_customer" => "New Customer",
+    "all_customer" => "All Customers",
 
-    "product_color"=>"Product Color",
-    "new_product_color"=>"New Product Color",
-    "all_product_color"=>"All Product Color",
+    "product_color" => "Product Color",
+    "new_product_color" => "New Product Color",
+    "all_product_color" => "All Product Color",
 
-    "active"=>"Active",
+    "active" => "Active",
 
->>>>>>> 9d3f5bb4489650401a5c71e72f17af1ae411e306
 ];
